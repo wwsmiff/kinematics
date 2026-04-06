e36e9c1b42b072f874087f80d66ea38e3c4192e1
wwsmiff <arnav0872@gmail.com>
2026-04-06 21:28:55 +0530

    Make camera much better, and save object positions as json, add nlohmann::json

---
+++ include/camera.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+struct Camera {
+  float yaw{-90.0f};
+  float pitch{};
+  float distance{15.0f};
+  glm::vec3 target{0.0f, 0.0f, 0.0f};
+  glm::vec3 up{0.0f, 1.0f, 0.0f};
+  glm::vec3 front{0.0f, 0.0f, -1.0f};
+  glm::vec3 offset{};
+  float xtheta{};
+  float ytheta{};
+
+  glm::mat4 view() {
+    glm::vec3 pos = position();
+    return glm::lookAt(pos, glm::vec3{}, up);
+  }
+
+  glm::vec3 direction() {
+    glm::vec3 dir{};
+    dir.x = std::cosf(glm::radians(xtheta)) * std::cosf(glm::radians(ytheta));
+    dir.z = std::sinf(glm::radians(xtheta)) * std::cosf(glm::radians(ytheta));
+    dir.y = std::sinf(glm::radians(ytheta));
+
+    return dir;
+  }
+
+  glm::vec3 position() { return distance * direction(); }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-/* clang-format off */
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-/* clang-format on */
 #include "buffer_object.hpp"
+#include "camera.hpp"
 #include "shader.hpp"
+#include <GLFW/glfw3.h>
 #include <cassert>
 #include <charconv>
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
 #include <functional>
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 #include <memory>
 #include <numbers>
 #include <print>
@@ -21,14 +21,12 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "imgui.h"
+#include "nlohmann/json.hpp"
 
 using Window = std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow *)>>;
 
 namespace fs = std::filesystem;
 
-static float g_camera_yaw{-90.0f};
-static float g_camera_pitch{};
-static float g_camera_distance{10.0f};
 double g_mouse_x{}, g_mouse_y{};
 uint8_t g_stencil{};
 uint8_t g_selected_object{};
@@ -40,27 +38,140 @@ float g_x_translation{}, g_y_translation{}, g_z_translation{};
 glm::mat4 view{};
 glm::mat4 projection{};
 
+Camera camera{};
+
+std::vector<glm::vec3> parse_objfile(const fs::path &path) {
+  std::vector<glm::vec3> loaded_vertices{};
+  auto split = [](const std::string &s,
+                  const char d) -> std::vector<std::string> {
+    std::vector<std::string> res{};
+    std::string tmp{};
+    size_t idx{};
+    while (idx < s.size()) {
+      if (d == ' ') {
+        if (std::isspace(s.at(idx))) {
+          res.push_back(tmp);
+          tmp.clear();
+        } else {
+          tmp.push_back(s.at(idx));
+        }
+      } else {
+        if (s.at(idx) == d) {
+          res.push_back(tmp);
+          tmp.clear();
+        } else {
+          tmp.push_back(s.at(idx));
+        }
+      }
+      idx++;
+    }
+
+    res.push_back(tmp);
+
+    return res;
+  };
+
+  std::ifstream objfile{path};
+  std::string line{};
+  if (!fs::exists(path)) {
+    std::println(stderr, "File '{}' does not exist.", path.c_str());
+  }
+
+  std::vector<uint32_t> indices{};
+  std::vector<glm::vec3> vertices{};
+  while (std::getline(objfile, line)) {
+    const auto tokens = split(line, ' ');
+    const std::string type = tokens.at(0);
+    float x{}, y{}, z{}, w{1.0f};
+    if (type == "v") {
+      if (tokens.size() < 4) {
+        std::println(stderr, "Not enough coordinates for vertex.");
+      }
+      std::from_chars(tokens.at(1).data(),
+                      tokens.at(1).data() + tokens.at(1).size(), x);
+      std::from_chars(tokens.at(2).data(),
+                      tokens.at(2).data() + tokens.at(2).size(), y);
+      std::from_chars(tokens.at(3).data(),
+                      tokens.at(3).data() + tokens.at(3).size(), z);
+      vertices.emplace_back(x, y, z);
+    } else if (type == "f") {
+      if (tokens.size() < 2) {
+        std::println(stderr, "Not enough indices for face.");
+      }
+      for (const auto &token : tokens) {
+        if (token == "f") {
+          continue;
+        } else {
+          if (token.find('/') != std::string::npos) {
+            uint32_t idx{};
+            auto face_data = split(token, '/');
+            std::from_chars(face_data.at(0).data(),
+                            face_data.at(0).data() + face_data.at(0).size(),
+                            idx);
+            indices.push_back(idx - 1);
+          }
+        }
+      }
+    }
+  }
+
+  if (indices.size() % 3 != 0) {
+    std::println(stderr, "Not enough vertices.");
+  } else {
+    for (size_t i{}; i < indices.size(); i++) {
+      glm::vec3 v = vertices.at(indices.at(i));
+      loaded_vertices.push_back(v);
+    }
+  }
+  return loaded_vertices;
+}
+
 struct Mesh {
-  int32_t id{g_object_id};
+  uint32_t id{};
   std::vector<glm::vec3> vertices{};
   std::vector<uint32_t> indices{};
   glm::mat4 model{1.0f};
   glm::mat4 position{1.0f};
   glm::mat4 rotation{1.0f};
 
-  void draw() {
-    glStencilFunc(GL_ALWAYS, g_object_id, 0xFF);
+  uint32_t vao{};
+  BufferObject<glm::vec3> vbo{};
+
+  void load_from_objfile(const fs::path &path) {
+    id = g_object_id++;
+    vertices = parse_objfile(path);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    vbo.bind(BufferObjectType::Array, vertices);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 1 * sizeof(glm::vec3),
+                          (void *)(0));
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    std::println("{} vertices loaded.", vertices.size());
+  }
+
+  void draw(ShaderProgram &shader) {
+    if (id == 0) {
+      std::println(stderr, "Mesh id must be non-zero.");
+      return;
+    }
+    glBindVertexArray(vao);
+    glStencilFunc(GL_ALWAYS, id, 0xFF);
+    shader.use();
+    shader.set_matrix4x4f("model", glm::value_ptr(model));
     if (indices.empty()) {
       glDrawArrays(GL_TRIANGLES, 0, vertices.size());
     } else {
       glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
     }
+    glBindVertexArray(0);
   }
 };
 
 Mesh placeholder_mesh{};
-
 Mesh *g_selected_mesh{&placeholder_mesh};
+std::vector<Mesh *> g_meshes{};
 
 constexpr uint32_t window_width{1280};
 constexpr uint32_t window_height{720};
@@ -89,12 +200,15 @@ void framebuffer_size_callback(GLFWwindow *window, int32_t width,
 void mouse_move_callback(GLFWwindow *window, double curr_mouse_x,
                          double curr_mouse_y) {
   if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS) {
-    g_camera_yaw += (curr_mouse_x - g_mouse_x) * 0.1f;
-    g_camera_pitch -= (g_mouse_y - curr_mouse_y) * 0.1f;
+    camera.xtheta += (curr_mouse_x - g_mouse_x) * 0.1f;
+    camera.ytheta -= (g_mouse_y - curr_mouse_y) * 0.1f;
   }
 
-  if (g_editor_mode == EditorMode::Rotation && g_selected_object > 0 &&
-      g_manipulation_axis == ManipulationAxis::X) {
+  if (g_editor_mode == EditorMode::Normal) {
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+    }
+  } else if (g_editor_mode == EditorMode::Rotation && g_selected_object > 0 &&
+             g_manipulation_axis == ManipulationAxis::X) {
     float x_theta = glm::radians(static_cast<float>(curr_mouse_y - g_mouse_y));
     g_x_theta += glm::degrees(x_theta);
     g_x_theta = std::fmod(g_x_theta, 360.0f);
@@ -161,99 +275,17 @@ void mouse_move_callback(GLFWwindow *window, double curr_mouse_x,
   float n_mouse_y = ((window_height / 2) - g_mouse_y) / (window_height);
 }
 
+void mouse_scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+  camera.distance = ((camera.distance - yoffset) > 0.3f)
+                        ? (camera.distance - yoffset)
+                        : (camera.distance);
+}
+
 void mouse_button_callback(GLFWwindow *window, int button, int action,
                            int mods) {
   if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
     g_selected_object = g_stencil;
   }
-}
-
-Mesh parse_objfile(const fs::path &path) {
-  auto split = [](const std::string &s,
-                  const char d) -> std::vector<std::string> {
-    std::vector<std::string> res{};
-    std::string tmp{};
-    size_t idx{};
-    while (idx < s.size()) {
-      if (d == ' ') {
-        if (std::isspace(s.at(idx))) {
-          res.push_back(tmp);
-          tmp.clear();
-        } else {
-          tmp.push_back(s.at(idx));
-        }
-      } else {
-        if (s.at(idx) == d) {
-          res.push_back(tmp);
-          tmp.clear();
-        } else {
-          tmp.push_back(s.at(idx));
-        }
-      }
-      idx++;
-    }
-
-    res.push_back(tmp);
-
-    return res;
-  };
-
-  std::ifstream objfile{path};
-  std::string line{};
-  if (!fs::exists(path)) {
-    std::println(stderr, "File '{}' does not exist.", path.c_str());
-  }
-
-  Mesh m{};
-  std::vector<uint32_t> indices{};
-  std::vector<glm::vec3> vertices{};
-  while (std::getline(objfile, line)) {
-    const auto tokens = split(line, ' ');
-    const std::string type = tokens.at(0);
-    float x{}, y{}, z{}, w{1.0f};
-    if (type == "v") {
-      if (tokens.size() < 4) {
-        std::println(stderr, "Not enough coordinates for vertex.");
-      }
-      std::from_chars(tokens.at(1).data(),
-                      tokens.at(1).data() + tokens.at(1).size(), x);
-      std::from_chars(tokens.at(2).data(),
-                      tokens.at(2).data() + tokens.at(2).size(), y);
-      std::from_chars(tokens.at(3).data(),
-                      tokens.at(3).data() + tokens.at(3).size(), z);
-      std::println("new vertex pushed {}, {}, {}", x, y, z);
-      vertices.emplace_back(x, y, z);
-    } else if (type == "f") {
-      if (tokens.size() < 2) {
-        std::println(stderr, "Not enough indices for face.");
-      }
-      for (const auto &token : tokens) {
-        if (token == "f") {
-          continue;
-        } else {
-          if (token.find('/') != std::string::npos) {
-            uint32_t idx{};
-            auto face_data = split(token, '/');
-            std::from_chars(face_data.at(0).data(),
-                            face_data.at(0).data() + face_data.at(0).size(),
-                            idx);
-            indices.push_back(idx - 1);
-          }
-        }
-      }
-    }
-  }
-
-  if (indices.size() % 3 != 0) {
-    std::println(stderr, "Not enough vertices.");
-  } else {
-    for (size_t i{}; i < indices.size(); i++) {
-      glm::vec3 v = vertices.at(indices.at(i));
-      std::println("Vertex {}, {}, {}", v.x, v.y, v.z);
-      m.vertices.push_back(v);
-    }
-  }
-  return m;
 }
 
 std::string format_rotation(char axis, float theta) {
@@ -310,6 +342,10 @@ std::string format_translation() {
 }
 
 int main() {
+  std::ifstream object_states_in("object_states.json");
+  nlohmann::json matrix_data{};
+  object_states_in >> matrix_data;
+
   if (!glfwInit()) {
     std::println(stderr, "Failed to initialize GLFW.");
     return 1;
@@ -346,8 +382,8 @@ int main() {
   ImGui::CreateContext();
 
   ShaderProgram basic_shader{};
-  basic_shader.load("shaders/shader.vert", ShaderType::Vertex);
-  basic_shader.load("shaders/shader.frag", ShaderType::Fragment);
+  basic_shader.load("shaders/basic.vert", ShaderType::Vertex);
+  basic_shader.load("shaders/basic.frag", ShaderType::Fragment);
   basic_shader.link();
 
   ShaderProgram wireframe_shader{};
@@ -360,31 +396,58 @@ int main() {
   selection_shader.load("shaders/selection.frag", ShaderType::Fragment);
   selection_shader.link();
 
-  Mesh cube = parse_objfile("./RoboticArm/endeffector.obj");
+  // TODO: Convert to filesystem path glob and use std::vector to handle meshes.
+  Mesh endeffector{};
+  endeffector.load_from_objfile("./RoboticArm/endeffector.obj");
+  g_meshes.push_back(&endeffector);
 
-  uint32_t vao{};
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
+  Mesh base{};
+  base.load_from_objfile("./RoboticArm/base.obj");
+  g_meshes.push_back(&base);
 
-  BufferObject<glm::vec3> vbo{};
-  vbo.bind(BufferObjectType::Array, cube.vertices);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 1 * sizeof(glm::vec3),
-                        (void *)(0));
-  glEnableVertexAttribArray(0);
+  Mesh segment1{};
+  segment1.load_from_objfile("./RoboticArm/segment1.obj");
+  g_meshes.push_back(&segment1);
 
-  BufferObject<uint32_t> ebo{};
-  ebo.bind(BufferObjectType::Element, cube.indices);
+  Mesh segment2{};
+  segment2.load_from_objfile("./RoboticArm/segment2.obj");
+  g_meshes.push_back(&segment2);
 
-  glm::vec3 camera_pos{0.0f, 0.0f, 3.0f};
-  glm::vec3 camera_target{0.0f, 0.0f, 0.0f};
-  glm::vec3 camera_direction = glm::normalize(camera_pos - camera_target);
+  Mesh segment3{};
+  segment3.load_from_objfile("./RoboticArm/segment3.obj");
+  g_meshes.push_back(&segment3);
 
-  glm::vec3 world_up = {0.0f, 1.0f, 0.0f};
-  glm::vec3 camera_right =
-      glm::normalize(glm::cross(world_up, camera_direction));
-  glm::vec3 camera_up = glm::cross(camera_direction, camera_right);
-  glm::vec3 camera_front{0.0f, 0.0f, -1.0f};
-  glm::vec3 direction{};
+  std::vector<float> flattened_matrix{};
+
+  matrix_data["endeffector"]["position"].get_to(flattened_matrix);
+  endeffector.position = glm::make_mat4(flattened_matrix.data());
+  matrix_data["endeffector"]["rotation"].get_to(flattened_matrix);
+  endeffector.rotation = glm::make_mat4(flattened_matrix.data());
+  endeffector.model = endeffector.position * endeffector.rotation;
+
+  matrix_data["base"]["position"].get_to(flattened_matrix);
+  base.position = glm::make_mat4(flattened_matrix.data());
+  matrix_data["base"]["rotation"].get_to(flattened_matrix);
+  base.rotation = glm::make_mat4(flattened_matrix.data());
+  base.model = base.position * base.rotation;
+
+  matrix_data["segment1"]["position"].get_to(flattened_matrix);
+  segment1.position = glm::make_mat4(flattened_matrix.data());
+  matrix_data["segment1"]["rotation"].get_to(flattened_matrix);
+  segment1.rotation = glm::make_mat4(flattened_matrix.data());
+  segment1.model = segment1.position * segment1.rotation;
+
+  matrix_data["segment2"]["position"].get_to(flattened_matrix);
+  segment2.position = glm::make_mat4(flattened_matrix.data());
+  matrix_data["segment2"]["rotation"].get_to(flattened_matrix);
+  segment2.rotation = glm::make_mat4(flattened_matrix.data());
+  segment2.model = segment2.position * segment2.rotation;
+
+  matrix_data["segment3"]["position"].get_to(flattened_matrix);
+  segment3.position = glm::make_mat4(flattened_matrix.data());
+  matrix_data["segment3"]["rotation"].get_to(flattened_matrix);
+  segment3.rotation = glm::make_mat4(flattened_matrix.data());
+  segment3.model = segment3.position * segment3.rotation;
 
   projection = glm::perspective(glm::radians(45.0f),
                                 static_cast<float>(window_width) /
@@ -393,6 +456,7 @@ int main() {
 
   glfwSetCursorPosCallback(window.get(), mouse_move_callback);
   glfwSetMouseButtonCallback(window.get(), mouse_button_callback);
+  glfwSetScrollCallback(window.get(), mouse_scroll_callback);
   ImGui_ImplOpenGL3_Init();
   ImGui_ImplGlfw_InitForOpenGL(window.get(), true);
 
@@ -451,32 +515,33 @@ int main() {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    glBindVertexArray(vao);
-
-    view = glm::lookAt(camera_pos, glm::vec3{}, camera_up);
+    view = glm::translate(camera.view(), camera.offset);
 
     // wireframe
     wireframe_shader.use();
-    wireframe_shader.set_matrix4x4f("model", glm::value_ptr(cube.model));
     wireframe_shader.set_matrix4x4f("view", glm::value_ptr(view));
     wireframe_shader.set_matrix4x4f("projection", glm::value_ptr(projection));
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    cube.draw();
+    for (size_t i{}; i < g_meshes.size(); ++i) {
+      g_meshes.at(i)->draw(wireframe_shader);
+    }
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-    if (g_selected_object == cube.id) {
-      g_selected_mesh = &cube;
-      selection_shader.use();
-      selection_shader.set_matrix4x4f("model", glm::value_ptr(cube.model));
-      selection_shader.set_matrix4x4f("view", glm::value_ptr(view));
-      selection_shader.set_matrix4x4f("projection", glm::value_ptr(projection));
-      cube.draw();
-    } else {
-      basic_shader.use();
-      basic_shader.set_matrix4x4f("model", glm::value_ptr(cube.model));
-      basic_shader.set_matrix4x4f("view", glm::value_ptr(view));
-      basic_shader.set_matrix4x4f("projection", glm::value_ptr(projection));
-      cube.draw();
+    selection_shader.use();
+    selection_shader.set_matrix4x4f("view", glm::value_ptr(view));
+    selection_shader.set_matrix4x4f("projection", glm::value_ptr(projection));
+
+    basic_shader.use();
+    basic_shader.set_matrix4x4f("view", glm::value_ptr(view));
+    basic_shader.set_matrix4x4f("projection", glm::value_ptr(projection));
+
+    for (size_t i{}; i < g_meshes.size(); ++i) {
+      if (g_selected_object == g_meshes.at(i)->id) {
+        g_selected_mesh = g_meshes.at(i);
+        g_meshes.at(i)->draw(selection_shader);
+      } else {
+        g_meshes.at(i)->draw(basic_shader);
+      }
     }
 
     ImGui::Render();
@@ -486,20 +551,17 @@ int main() {
     glfwSwapBuffers(window.get());
     glfwPollEvents();
 
-    glm::vec3 direction{};
-    direction.x = std::cosf(glm::radians(g_camera_yaw)) *
-                  std::cosf(glm::radians(g_camera_pitch));
-    direction.z = std::sinf(glm::radians(g_camera_yaw)) *
-                  std::cosf(glm::radians(g_camera_pitch));
-    direction.y = std::sinf(glm::radians(g_camera_pitch));
-    camera_pos = g_camera_distance * direction;
-    camera_front = glm::normalize(glm::vec3{} - camera_pos);
-
     // process input
     if (glfwGetKey(window.get(), GLFW_KEY_W) == GLFW_PRESS) {
-      g_camera_distance -= 0.1f;
+      camera.offset.y -= 0.1f;
     } else if (glfwGetKey(window.get(), GLFW_KEY_S) == GLFW_PRESS) {
-      g_camera_distance += 0.1f;
+      camera.offset.y += 0.1f;
+    }
+
+    if (glfwGetKey(window.get(), GLFW_KEY_A) == GLFW_PRESS) {
+      camera.offset.z -= 0.1f;
+    } else if (glfwGetKey(window.get(), GLFW_KEY_D) == GLFW_PRESS) {
+      camera.offset.z += 0.1f;
     }
 
     if (g_editor_mode == EditorMode::Normal) {
@@ -553,6 +615,43 @@ int main() {
   }
 
   glfwTerminate();
+
+  auto flatten_model_matrix = [](const glm::mat4 &m) -> std::vector<float> {
+    return std::vector<float>(glm::value_ptr(m), glm::value_ptr(m) + 16);
+  };
+
+  nlohmann::json object_states_data = {
+      {"endeffector",
+       {
+           {"position", flatten_model_matrix(endeffector.position)},
+           {"rotation", flatten_model_matrix(endeffector.rotation)},
+       }},
+
+      {"base",
+       {
+           {"position", flatten_model_matrix(base.position)},
+           {"rotation", flatten_model_matrix(base.rotation)},
+       }},
+      {"segment1",
+       {
+           {"position", flatten_model_matrix(segment1.position)},
+           {"rotation", flatten_model_matrix(segment1.rotation)},
+       }},
+      {"segment2",
+       {
+           {"position", flatten_model_matrix(segment2.position)},
+           {"rotation", flatten_model_matrix(segment2.rotation)},
+       }},
+      {"segment3",
+       {
+           {"position", flatten_model_matrix(segment3.position)},
+           {"rotation", flatten_model_matrix(segment3.rotation)},
+       }},
+
+  };
+
+  std::ofstream object_states_out("object_states.json");
+  object_states_out << object_states_data << std::endl;
 
   return 0;
 }
